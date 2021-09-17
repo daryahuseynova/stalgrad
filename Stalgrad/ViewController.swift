ee0a20807dafc75f2608be49522e04b3caa23b5e
Darya Huseynova <daryahuseynova03@gmail.com>
2021-09-17 15:43:26 +0300

    seque

--- Stalgrad/ViewController.swift
+++ Stalgrad/ViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    
+    @IBAction func continueButton(_ sender: UIButton) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
