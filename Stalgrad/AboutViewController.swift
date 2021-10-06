1d3a24923d24670999a0c07c7fce07a6506a0f5f
Darya Huseynova <daryahuseynova03@gmail.com>
2021-10-06 19:40:30 +0300

    tab bar edited

---
+++ Stalgrad/AboutViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AboutViewController.swift
+//  Stalgrad
+//
+//  Created by Darya Huseynova on 1.10.21.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
